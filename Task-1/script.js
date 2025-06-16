d7743a62b2cc221bb8ff217d0b66ea47eba729f0
Pratibha <rpratibha901@gmail.com>
2025-06-16 22:13:22 +0530

    Task 2

---
+++ Task-1/script.js
@@ -0,0 +1,19 @@
+// const head = document.querySelector('#head');
+// const head = document.getElementById('head');
+
+// console.log(head)
+
+// head.style.color="red";
+
+// head.style.border="2px solid green";
+
+// head.style.backgroundColor="blue";
+
+const btn = document.getElementById('btn');
+
+bar.style.margin-top
+
+btn.addEventListener('Click',()=>{
+    head.style.backgroundColor="grey";
+})
+
